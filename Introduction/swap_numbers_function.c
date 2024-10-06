9c0bd23517606ec60671ebfa7bc01bacf7cebaac
KayongoYongo <sknsammie10@gmail.com>
2024-10-06 18:02:39 +0300

    Swap numbers function

---
+++ Introduction/swap_numbers_function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int num1, num2);
+{
+	int temp;
+	temp = num1;
+	num1 = num2;
+	num2 = temp;
+}
+
+void main()
+{
+	int a = 5;
+	int b = 9;
+	printf("Before swap, num1 is %d and num2 is %d/n", a, b);
+	swap(a, b);
+	printf("After swap, num1 is %d and num2 is %d/n", a, b):
+}
